c6feee1447c550da64da5442cff9f89bbbf56154
cassiestephen <cassie.v.stephen@gmail.com>
2023-11-12 15:25:21 -0800

    Sign up Page Fully Implemented with Basic CSS

--- src/components/sign-up.js
+++ src/components/sign-up.js
@@ -1,9 +1,39 @@
+import myEngineering from "./images/my-engineering.png";
+
 function SignUp() {
-    return (
-      <div >
-        heya
+  return (
+    <div className="sign-up-page">
+      <div className="header-sign-up">
+        <div className="header-text-sign-up">
+        Sign Up to be a 2024-2025 mentorSEAS Mentor!
+        </div>
       </div>
-    );
-  }
-  
-  export default SignUp;
+      <div className="subheader-sign-up">
+        ** applications will become available April 2024
+      </div>
+      <div className="hstack-sign-up">
+        <div className="vstack-sign-up">
+        <div className="sign-up-info-header">How do I sign up?</div>
+      <div className="sign-up-info-subheader">It's super easy!</div>
+          <text className="text-sign-up">
+            1. Start by logging into your myEngineering account. Go to the
+            "About Me" section as shown in the image.
+          </text>
+          <text className="text-sign-up">
+            2. If the application period is open, there will be a "Mentor
+            Profile" section. Fill in the information, agree to the agreements
+            and click "Sign up to be a mentor".
+          </text>
+          <text className="text-sign-up">
+            3. Fill out the accompanying Mentee Matching survey, linked in
+            myEngineering.
+          </text>
+        </div>
+        <img className="my-engr-image" src={myEngineering} />
+        </div>
+        <div className="bottom-text-sign-up">Once you've completed the steps above, you're all set!</div>
+    </div>
+  );
+}
+
+export default SignUp;
